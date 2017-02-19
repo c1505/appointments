34cfae7df49658cf4a847c4b8dc6352934b96456
c1505 <coreym247@gmail.com>
2017-02-18 19:41:10 -0500

    rendering template

--- appointments/views.py
+++ appointments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse
-
+from django.shortcuts import render
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request,"appointments/index.html", {})
+# will be returning json here i belive
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mtvelkq%t41bb*m)3h-p^f0(%2a)4mwrhsv=*l6w6tc*+97nwu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appointments',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
