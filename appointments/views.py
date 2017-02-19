33ec7220f9becc47fa9282b2ff78de5cd0a93099
c1505 <coreym247@gmail.com>
2017-02-18 21:43:11 -0500

    form posting correctly

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-# from .forms import NameForm
+from .forms import AppointmentForm
+from django.http import HttpResponse
 def index(request):
     return render(request,"appointments/index.html", {})
+
+def create(request):
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            return render(request,"appointments/index.html", {})
+        else:
+            return HttpResponse("Invalid Form")
 
 
 # will be returning json here i belive
