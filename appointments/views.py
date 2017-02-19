20006a13ff97a04fccfebdb54ebd8fb0011f2e11
c1505 <coreym247@gmail.com>
2017-02-18 22:55:52 -0500

    including javascript

--- appointments/views.py
+++ appointments/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .forms import AppointmentForm
 from django.http import HttpResponse
+import pdb
 def index(request):
     return render(request,"appointments/index.html", {})
 
 def create(request):
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
+        # pdb.set_trace()
+        # datetime = request.POST['datetime']
+        # description = request.POST['description']
         if form.is_valid():
             return render(request,"appointments/index.html", {})
         else:
