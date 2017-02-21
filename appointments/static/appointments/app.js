1b2b862a530898af0eeeeadd6b9248b13041199b
c1505 <coreym247@gmail.com>
2017-02-20 23:36:36 -0500

    for..in incompatibility with Internet Explorer.  Replaced with traditional for loop.

--- appointments/static/appointments/app.js
+++ appointments/static/appointments/app.js
@@ -1,30 +1,33 @@
 $( document ).ready(function() {
   $("#new").click( function(event) {
     $(".toggleHide").toggleClass("hidden");
-  })
+  });
   $("#cancel").click( function(event) {
     $(".toggleHide").toggleClass("hidden");
-  })
+  });
 
   $("#search").click( function(event) {
     $(".added").html("");
     var params = $("#searchInput").val();
     event.preventDefault();
     getAppointments(params);
-    $("#searchInput").val("")
+    $("#searchInput").val("");
     });
-})
+});
+
+
 
 function getAppointments(params) {
   $.get( "search/", {params: params}, function( data ) {
-    var rows = ""
-    for (appointment of data.appointments) {
-
-      var date = new Date(appointment.datetime);
-      var date = ($.format.date(appointment.datetime, "MMM d"))
-      var time = ($.format.date(appointment.datetime, "h:mmp"))
-      var row = "<tr class='added'><td>" + date + "</td><td>" + time + "</td><td>"+ appointment.description + "</td></tr>"
-      rows = rows + row
+    var appointments = data.appointments;
+    var appointmentsLength = appointments.length;
+    var rows = "";
+    for (var i = 0; i < appointmentsLength; i++) {
+      var date = new Date(appointments[i].datetime);
+      var date = ($.format.date(appointments[i].datetime, "MMM d"));
+      var time = ($.format.date(appointments[i].datetime, "h:mmp"));
+      var row = "<tr class='added'><td>" + date + "</td><td>" + time + "</td><td>"+ appointments[i].description + "</td></tr>";
+      rows = rows + row;
     }
     $("#results").after(rows);
   });
